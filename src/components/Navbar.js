6088bbc0b309ff94eb33c1152808c56083293dc2
Realsammy <realsammy86@gmail.com>
2023-03-26 17:18:01 +0100

    added auth

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='nav'>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact us</li>
+            <li>Login</li>
+            <li>Logout</li>
+            <li>Cart</li>
+        </ul>
+       
+    </div>
+  )
+}
+
+export default Navbar
